680db67396569f87c401a9a332d038b12ad7abe3
raman45789 <166947119+raman45789@users.noreply.github.com>
2024-04-14 01:22:32 +0530

    Add files via upload

---
+++ recipe.js
@@ -0,0 +1,28 @@
+function toggleIngredients1() 
+{
+    var ingredients = document.getElementById("ingredients1");
+    if (ingredients.style.display === "none") {
+         ingredients.style.display = "block";
+        } 
+    else {
+                ingredients.style.display = "none";
+        }
+}
+function toggleIngredients2() {
+    var ingredients = document.getElementById("ingredients2");
+    if (ingredients.style.display === "none") {
+        ingredients.style.display = "block";
+       } 
+    else {
+                ingredients.style.display = "none";
+        }
+}
+function toggleIngredients3() {
+    var ingredients = document.getElementById("ingredients3");
+    if (ingredients.style.display === "none") {
+        ingredients.style.display = "block";
+       } 
+    else {
+                ingredients.style.display = "none";
+        }
+}
